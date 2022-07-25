53b63719e32a6af432a3a3365a19c72dae75c8b6
Sheikhi Maryam <SHEIKHI@kapsch.net>
2022-07-25 09:54:39 +0200

    Fouth Commith

--- Tests/Tests/Program.cs
+++ Tests/Tests/Program.cs
@@ -7,6 +7,7 @@ namespace Tests
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Forth commit");
         }
     }
 }
